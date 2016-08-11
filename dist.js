7d77fa55331a880e02a6a6f5a203572453c82206
Viljo Viitanen <viljo.viitanen@gmail.com>
2016-08-11 21:53:59 +0300

    Create dist.js

---
+++ dist.js
@@ -0,0 +1,21 @@
+a=message.suffix.split(',')
+sys1=a[0]
+sys2=a[1]
+unirest.get('http://www.edsm.net/api-v1/system?sysname='+sys1+'&coords=1')
+.end(function (r1) {
+  unirest.get('http://www.edsm.net/api-v1/system?sysname='+sys2+'&coords=1')
+    .end(function (r2) {
+    x1=r1.body.coords.x
+    y1=r1.body.coords.y
+    z1=r1.body.coords.z
+    x2=r2.body.coords.x
+    y2=r2.body.coords.y
+    z2=r2.body.coords.z
+    dx=x1-x2
+    dy=y1-y2
+    dz=z1-z2
+    dist=Math.sqrt(dx*dx+dy*dy+dz*dz).toFixed(1)
+    r="Distance between "+sys1+" and "+sys2+" is "+dist
+    ch.sendMessage(r);
+  });
+});
