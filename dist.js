caece23eb4d4e3cd4a5a42d509bc8edb94419b2a
Viljo Viitanen <viljo.viitanen@gmail.com>
2016-08-12 10:31:44 +0300

    Update dist.js
    add url encode to params sent to edsm

--- dist.js
+++ dist.js
@@ -1,10 +1,10 @@
 a=message.suffix.split(',')
 sys1=a[0].trim()
 sys2=a[1].trim()
-unirest.get('http://www.edsm.net/api-v1/system?sysname='+sys1+'&coords=1')
+unirest.get('http://www.edsm.net/api-v1/system?sysname='+encodeURIComponent(sys1)+'&coords=1')
 .end(function (r1) {
   if('coords' in r1.body) {
-    unirest.get('http://www.edsm.net/api-v1/system?sysname='+sys2+'&coords=1')
+    unirest.get('http://www.edsm.net/api-v1/system?sysname='+encodeURIComponent(sys2)+'&coords=1')
       .end(function (r2) {
         if('coords' in r2.body) {
           x1=r1.body.coords.x
@@ -17,7 +17,7 @@ unirest.get('http://www.edsm.net/api-v1/system?sysname='+sys1+'&coords=1')
           dy=y1-y2
           dz=z1-z2
           dist=Math.sqrt(dx*dx+dy*dy+dz*dz).toFixed(1)
-          r="Distance between "+sys1+" and "+sys2+" is "+dist
+          r="Distance between "+sys1+" and "+sys2+" is "+dist+ "ly"
         }
         else {
           r=sys2+" coordinates not found"
