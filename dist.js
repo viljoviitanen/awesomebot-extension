1c99a5c4e16f134d9bd821fed3d1a709a6f18393
Viljo Viitanen <viljo.viitanen@gmail.com>
2016-08-12 10:14:39 +0300

    Update dist.js
    add some error handling

--- dist.js
+++ dist.js
@@ -3,19 +3,28 @@ sys1=a[0].trim()
 sys2=a[1].trim()
 unirest.get('http://www.edsm.net/api-v1/system?sysname='+sys1+'&coords=1')
 .end(function (r1) {
-  unirest.get('http://www.edsm.net/api-v1/system?sysname='+sys2+'&coords=1')
-    .end(function (r2) {
-    x1=r1.body.coords.x
-    y1=r1.body.coords.y
-    z1=r1.body.coords.z
-    x2=r2.body.coords.x
-    y2=r2.body.coords.y
-    z2=r2.body.coords.z
-    dx=x1-x2
-    dy=y1-y2
-    dz=z1-z2
-    dist=Math.sqrt(dx*dx+dy*dy+dz*dz).toFixed(1)
-    r="Distance between "+sys1+" and "+sys2+" is "+dist
-    ch.sendMessage(r);
-  });
+  if('coords' in r1.body) {
+    unirest.get('http://www.edsm.net/api-v1/system?sysname='+sys2+'&coords=1')
+      .end(function (r2) {
+        if('coords' in r2.body) {
+          x1=r1.body.coords.x
+          y1=r1.body.coords.y
+          z1=r1.body.coords.z
+          x2=r2.body.coords.x
+          y2=r2.body.coords.y
+          z2=r2.body.coords.z
+          dx=x1-x2
+          dy=y1-y2
+          dz=z1-z2
+          dist=Math.sqrt(dx*dx+dy*dy+dz*dz).toFixed(1)
+          r="Distance between "+sys1+" and "+sys2+" is "+dist
+        }
+        else {
+          r=sys2+" coordinates not found"
+        }
+        ch.sendMessage(r);
+    });
+  } else {
+    ch.sendMessage(sys1+" coordinates not found");
+  } 
 });
